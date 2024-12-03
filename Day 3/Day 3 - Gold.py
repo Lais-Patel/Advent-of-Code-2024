e4dd7af353a3eb394c6ead1bd304c2c940576ad9
Mango <125408093+Lais-Patel@users.noreply.github.com>
2024-12-03 18:21:08 +0000

    Day 3 Solution to Gold

--- Day 3/Day 3 - Gold.py
+++ Day 3/Day 3 - Gold.py
@@ -6,15 +6,24 @@ add = True
 with open("input_data.txt") as f:
     data += (f.read()).replace("\n","")
 
-def findfunction():
+def findfunction(add):
     count = data.find("mul(") + 4
+    doex = data.rfind("do()", 0, count)
+    dontex = data.rfind("don't()", 0, count)
+
+    if doex > dontex:
+        add = True
+    if dontex > doex:
+        add = False
+    print(f"Do({doex}) > Don't({dontex})) = {add}")
+
     y = data[count]
 
     while y.isdigit():
         count += 1
         y += data[count] 
      
-    if y.endswith(","):
+    if y.endswith(",") and add == True:
         y = y.split(",")
     else:
         return False
@@ -26,15 +35,15 @@ def findfunction():
         count += 1
         y[1] += data[count]
     
-    if y[1].endswith(")"):
+    if y[1].endswith(")") and add == True:
         y[1] = y[1].replace(")","")
         return y
     else:
         return False
     
 while data.find("mul(") != -1:
-    check = findfunction()
-    if check != False:
+    check = findfunction(add)
+    if check != False and add == True:
         list_.append(check)
     try:
         data = data.replace("mul(",".",1)
